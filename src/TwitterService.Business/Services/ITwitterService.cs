017f57e911a609d158aac00b82b4e713506361be
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-08-06 23:57:10 +0300

    spec flow added to project

---
+++ src/TwitterService.Business/Services/ITwitterService.cs
@@ -0,0 +1,8 @@
+﻿namespace TwitterService.Business.Services {
+    public interface ITwitterService {
+
+        void SearchKeywordsInTweets();
+        void AddSearchKeyword(string keyword);
+        void DisableSearchKeyword(string keyword);
+    }
+}
--- src/TwitterService.Business/Services/TwitterService.cs
+++ src/TwitterService.Business/Services/TwitterService.cs
@@ -2,6 +2,10 @@
 {
     public class TwitterService : BaseService
     {
+        public TwitterService() {
+            
+        }
+
 
     }
 }
